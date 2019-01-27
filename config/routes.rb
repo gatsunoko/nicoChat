a3c7753df461b2d74cec7cd2ee8666c10285895d
gatsuonrails <gatsuonrails@gmail.com>
2019-01-27 18:29:21 +0900

    puma設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
   resources :genres, only: [:index, :show]
   get 'home/index'
   root 'genres#index'
-  resources :users, :only => [:show, :edit, :update]
   devise_for :users, controllers: { registrations: 'users/registrations',
                                     confirmations: 'users/confirmations',
                                     sessions: "users/sessions",
                                     omniauth_callbacks: 'users/omniauth_callbacks' }
+  resources :users, :only => [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
