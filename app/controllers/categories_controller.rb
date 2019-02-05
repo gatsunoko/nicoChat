cef68b3df76ed65f434e37ec2d5517ebc71813a3
gatsuonrails <gatsuonrails@gmail.com>
2019-02-05 22:23:15 +0900

    トップページにチャットルーム一覧表示

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,10 @@
 class CategoriesController < ApplicationController
   def index
     @categories = Category.all
+    @rooms = Room.all.order(id: :desc).limit(100).offset(0)
   end
 
   def show
-    @rooms = Room.where(category_id: params[:id]).order(created_at: :desc)
+    @rooms = Room.where(category_id: params[:id]).order(id: :desc)
   end
 end
